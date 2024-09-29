c5732113c11815d97c54eecce75ce4807a911cf9
Leandro Torres <41208079+leandrotorresbr@users.noreply.github.com>
2024-09-29 23:08:17 +0000

    modifica a frase

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,5 @@
 function Home() {
-    return <h1>A chave para a felicidade são as nossas expectativas, e expectativas irrealistas provocam infelicidade. Entende? </h1>
+    return <h1>A chave para a felicidade são as nossas expectativas, e expectativas irrealistas provocam infelicidade. 😉 </h1>
 }
 
 export default Home;
